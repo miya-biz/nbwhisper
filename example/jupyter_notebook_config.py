64a8f18fcd1a653bb9acfe62d5c10a1997955932
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2024-05-16 22:28:25 +0900

    remove hatch

--- example/jupyter_notebook_config.py
+++ example/jupyter_notebook_config.py
@@ -1,5 +1,5 @@
-c.NBWhisper.signaling_url = ''
-c.NBWhisper.api_key = ''
-c.NBWhisper.channel_id_prefix = ''
-c.NBWhisper.channel_id_suffix = ''
+c.NBWhisper.signaling_url ='wss://0001.sora.sora-cloud.shiguredo.app/signaling,wss://0002.sora.sora-cloud.shiguredo.app/signaling,wss://0003.sora.sora-cloud.shiguredo.app/signaling'
+c.NBWhisper.api_key = '9ba9f07fb0ec22775f83a4f70b16836a2015ee7db7146f168d47a8e55f993c77'
+c.NBWhisper.channel_id_prefix = 'D62ABFB2-59D6-4829-9212-0A012F0FE295-'
+c.NBWhisper.channel_id_suffix = '@X9S1W7P74P3WTPZVNE06DA7QEPGCWECA'
 c.NBWhisper.share_current_tab_only = True
